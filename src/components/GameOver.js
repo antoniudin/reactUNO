eed14afacd87ee29000dfefd246531425f7b2ac5
Anton Iudin <yudin431@gmail.com>
2022-01-28 17:35:11 -0500

    Game Over Component

--- src/components/GameOver.js
+++ src/components/GameOver.js
@@ -1,16 +1,16 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useContext } from 'react';
 import PlayerContext from '../context/PlayerContext';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default function GameOver() {
   const playerContext = useContext(PlayerContext)
-  return <div>
-    GAME OVER
-      {playerContext.players.map(player=> 
-        <p key={player.id}>{player.name} <span>Score:</span> {player.score}</p>
-      )}   
-      <Link to="/desk">
-        <button>Start new game</button>
-      </Link>
+  return <div className="gameOver">
+            <div className="finalScore">
+              <img className="" src={require('../img/gameover.png')} />
+                {playerContext.players.map(player=> 
+                <p key={player.id}>{player.name} <span>Score:</span> {player.score}</p>
+              )}   
+            <Link to="/desk"><button className="startGameButton">Start new game</button></Link>
+          </div>
     </div>;
 }
