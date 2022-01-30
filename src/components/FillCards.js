a64a3be55f31de1039646e5cfbd715263b3a224f
Anton Iudin <yudin431@gmail.com>
2022-01-30 15:27:33 -0500

    Code refactored

--- src/components/FillCards.js
+++ src/components/FillCards.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export default function FillCards(colors) {
-    const values = ['0','1','2','3','4','5','6','7','8','9','S','+2','W','+4']
+    const values = ['W','0','1','2','3','4','5','6','7','8','9','S','R','+2','+4']
     let index = 1;
     let cards = []
     for (var i=0; i<values.length; i++) {
@@ -9,12 +9,12 @@ export default function FillCards(colors) {
             const card = {id:index, color:color,value:values[i]}
             cards.push(card)
             index++;
+            cards.push(card)
+            index++;
+            if (card.value=='W' || card.value=='+4' ) card.color="W";
         })
     }
-    cards.map(function(card){
-        if (card.value=='W' || card.value=='+4' ) {
-            card.color="W";
-        }
-    })
+    cards.splice(-4);
+    cards.splice(4,4)
     return cards; 
 }
