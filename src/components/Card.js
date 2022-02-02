33e9a24a6a1502902db37eafb834907186b4050a
Anton Iudin <yudin431@gmail.com>
2022-02-02 15:46:03 -0500

    Card component changed
    Hided player’s card when it’s not his turn

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { useEffect } from 'react/cjs/react.development';
 
 export default function Card(props) {
-  const {card, onClick} = props;
+const {card, onClick, flipped} = props;
 
-  return <div onClick={onClick} className={`newCard unoCard ${card.color}`}>
-            {card.value}
-            <div className='oval'></div>
-            <div className='mdl'>{card.value}</div>
-            <div className='mdl2'>{card.value}</div>
-            <div className='u_d'>{card.value}</div>
-        </div>;
+const Card = <div onClick={onClick} className={`newCard unoCard ${flipped ? card.color : null}`}>
+  {card.value}
+  <div className='oval'></div>
+  <div className='mdl'>{card.value}</div>
+  <div className='mdl2'>{card.value}</div>
+  <div className='u_d'>{card.value}</div>
+</div> 
+
+const FlippedCard = <div className='pile newCard red'>
+  <img className="angle" src={require('../img/unoCardBack.png')} />
+</div>
+
+  return flipped ? Card : FlippedCard;
 }
