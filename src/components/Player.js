d014f1092c54c9a54684a4b024b9f36947694bc6
Anton Iudin <yudin431@gmail.com>
2022-02-02 15:46:59 -0500

    Added flipped parameter

--- src/components/Player.js
+++ src/components/Player.js
@@ -3,18 +3,20 @@ import Card from './Card';
 
 class Player extends Component {
     render() { 
-        const {player, turn, nextTurn, onMakeTurn, onCompleteTurn, mainCard} = this.props;
+        const {player, turn, nextTurn, onMakeTurn, onCompleteTurn, onGrabCard, mainCard} = this.props;
         return (
             <div className='playerBoard'>
                 <div className='playerTitleLine'><div className={(turn===player.id ? 'active' : 'noActive')}>{player.name}</div>
                  {turn===player.id && <div className='playerIsGoing'>is going to make a turn</div>}</div>
                  <div className='scoreTitle'>Score: {player.score}</div>
                  
-                 <button onClick={onCompleteTurn.bind(this, player.id)}>Skip turn</button>
+                 {player.id==turn && <button className="customButton" onClick={onCompleteTurn.bind(this, player.id)}>Skip</button>}
+                 {player.id==turn && <button className="customButton" onClick={onGrabCard.bind(this, player.id)}>Grab a card</button>}
+                 {/* <button className="customButton">" UNO! "</button> */}
                 
                  <div className='playerCards'>
                  {player.cards.map(PlayerCard=>
-                    <Card key={PlayerCard.id} card={PlayerCard} onClick={onMakeTurn.bind(this, player.id, PlayerCard.id)}/>
+                    <Card flipped={player.id==turn ? true : false} key={PlayerCard.id} card={PlayerCard} onClick={onMakeTurn.bind(this, player.id, PlayerCard.id)}/>
                     )}
                     </div>
             </div>
