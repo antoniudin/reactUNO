eadc3a094c4f10d870cb18ed85e8a157f9f166be
Anton Iudin <yudin431@gmail.com>
2022-02-02 15:45:02 -0500

    About Page completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
+import {Route, Routes} from 'react-router-dom';
 import PlayerContext from './context/PlayerContext'
 import CardDesk from './components/CardDesk'
 import HomePage from './components/HomePage'
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import ErrorPage from './components/ErrorPage';
 import GameOver from './components/GameOver'
+import About from './components/About';
+
 
 class App extends Component {
   state = {
@@ -26,8 +29,10 @@ class App extends Component {
       <PlayerContext.Provider value={{players: this.state.players, onUpdate: this.handleUpdateScore, test: this.test}}
         >
           <Routes>
+            <Route path="/" element={<HomePage/>}/>
             <Route path="/game" exact element={<CardDesk/>}/>
             <Route path="/gameover" element={<GameOver/>}/>
+            <Route path="/about" element={<About/>}/>
             <Route path="/" element={<HomePage/>}/>
           </Routes>
       </PlayerContext.Provider>
---
+++ src/components/About.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import RedirectComponent from './RedirectComponent';
+
+export default function About() {
+  
+    const [data, setData] = useState(null);
+  
+  return <div className="homePage">
+      {data!=null && <RedirectComponent path={data}/>}
+      <div className='goButtonsContainer'>
+        <div className='gameOverHome' onClick = {()=> setData('')}></div>
+        <div className='gameOverPlay' onClick = {()=> setData('game')}></div>
+      </div>
+      <div className="center">
+      Here is the first version of my React JS UNO project where I implemented the basic functionality of the famous game. 
+      <br/><br/> 
+      I am not a designer and all the cards in this game 
+      were drawn by css only, so don't judge UI strictly.
+      
+      By now you manage all three players by hand,  
+      but I am making a plan to automate the process and make playing with the computer possible.<br/><br/>
+      
+      If you face an issue or notice an error,  I would like to hear about it. 
+      Also, if you like it, I don't mind to get a star from you on <span> </span>
+      <a href="https://github.com/antoniudin/reactUNO">GitHub</a> <br/><br/> Thank you!
+      </div>
+  </div>;
+}
